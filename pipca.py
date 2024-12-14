bd0ba1c24b778159c8d892473b05b77d02f068e9
Sergej392 <153978605+Sergej392@users.noreply.github.com>
2024-12-14 17:57:08 +0700

    Add files via upload

---
+++ pipca.py
@@ -0,0 +1,18 @@
+import pyautogui
+print("Введите login and password в окно")
+text=pyautogui.prompt(text="login and password",title="Текст",default="Ваш login and password")
+print(text)
+text=pyautogui.prompt(text="login and password",title="Текст",default="Ваш login and password")
+print(text)
+login = input()
+password = input()
+login = ("pipca")
+password = ("233232")
+if login == login:
+    print
+if password == password:
+    print("Добро пожаловать, pipca")
+else:
+    print("логин или пароль неверен")
+print("Спасибо, что пользуетесь технологиями Umbrella Corporation!")
+    
