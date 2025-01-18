88e8626b946bcc6b96877e70b7990f910f5e0343
Sergej392 <153978605+Sergej392@users.noreply.github.com>
2025-01-18 17:59:39 +0700

    Add files via upload

---
+++ Pi.py
@@ -0,0 +1,9 @@
+x = "Pi"
+while True:
+    print("Введите пароль")
+    x = input()
+    if x == "Pi":
+        print("Вход успешен")
+        break
+    else:
+       print("Неверно!Введите снова")
