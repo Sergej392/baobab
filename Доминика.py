defc40cb0ea7986cbe6a170ee4c12f9987810783
Sergej392 <153978605+Sergej392@users.noreply.github.com>
2025-02-01 17:57:21 +0700

    Add files via upload

---
+++ Доминика.py
@@ -0,0 +1,12 @@
+#a = 0
+#b = 11
+s = 0
+d = 0
+f = 0
+while s != 'stop':
+    print("ввести оценки Доминика (1, 2, 3, 4 или 5).")
+    s = input()
+    if s == "stop":                
+        #break
+    d=d+int(s )
+    f=f+1
